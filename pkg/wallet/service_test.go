e038a732a720a4664ecaf71a43f13883aa621ede
gholib <gtrandnet@gmail.com>
2020-10-09 09:34:12 +0500

    init project

---
+++ pkg/wallet/service_test.go
@@ -0,0 +1,239 @@
+package wallet
+
+import (
+	"fmt"
+	"reflect"
+	"testing"
+
+	"github.com/gholib/wallet/pkg/types"
+	"github.com/google/uuid"
+)
+
+type testService struct {
+	*Service
+}
+
+type testAccount struct {
+	phone    types.Phone
+	balance  types.Money
+	payments []struct {
+		amount   types.Money
+		category types.PaymentCategory
+	}
+}
+
+var defaultTestAccount = testAccount{
+	phone:   "+992880806776",
+	balance: 10_000_00,
+	payments: []struct {
+		amount   types.Money
+		category types.PaymentCategory
+	}{{
+		amount:   1000_00,
+		category: "auto",
+	}},
+}
+
+func newTestService() *testService {
+	return &testService{Service: &Service{}}
+}
+
+func (s *testService) addAccountWithBalance(phone types.Phone, balance types.Money) (*types.Account, error) {
+	account, err := s.RegisterAccount(phone)
+
+	if err != nil {
+		return nil, fmt.Errorf("cant register account, error = %v", err)
+	}
+
+	err = s.Deposit(account.ID, balance)
+
+	if err != nil {
+		return nil, fmt.Errorf("cant deposit account, error = %v", err)
+	}
+
+	return account, nil
+}
+
+func (s *testService) addAcoount(data testAccount) (*types.Account, []*types.Payment, error) {
+	account, err := s.RegisterAccount(data.phone)
+	if err != nil {
+		return nil, nil, fmt.Errorf("cant register account %v = ", err)
+	}
+
+	err = s.Deposit(account.ID, data.balance)
+	if err != nil {
+		return nil, nil, fmt.Errorf("cant deposit account %v = ", err)
+	}
+
+	payments := make([]*types.Payment, len(data.payments))
+	for i, payment := range data.payments {
+		payments[i], err = s.Pay(account.ID, payment.amount, payment.category)
+		if err != nil {
+			return nil, nil, fmt.Errorf("cant make payment %v = ", err)
+		}
+	}
+
+	return account, payments, nil
+}
+
+func TestService_FindPaymentByID_success(t *testing.T) {
+	s := newTestService()
+
+	_, payments, err := s.addAcoount(defaultTestAccount)
+	if err != nil {
+		t.Error(err)
+		return
+	}
+
+	payment := payments[0]
+
+	got, err := s.FindPaymentByID(payment.ID)
+	if err != nil {
+		t.Errorf("FindPaymentId(), error %v", err)
+		return
+	}
+
+	if !reflect.DeepEqual(payment, got) {
+		t.Errorf("FindPaymentId(), wrong payment returned %v = ", got)
+	}
+}
+
+func TestService_FindPaymentByID_fail(t *testing.T) {
+	s := newTestService()
+
+	_, _, err := s.addAcoount(defaultTestAccount)
+	if err != nil {
+		t.Error(err)
+		return
+	}
+
+	_, err = s.FindPaymentByID(uuid.New().String())
+	if err == nil {
+		t.Errorf("FindPaymentId(), must return error, returned nil %v = ", err)
+		return
+	}
+
+	if err != ErrPaymentNotFound {
+		t.Errorf("FindPaymentId(), must return ErrPaymentNotFound returned %v = ", err)
+	}
+}
+
+func TestService_FindbyAccountById_success(t *testing.T) {
+	svc := Service{}
+	svc.RegisterAccount("+9929351007")
+	account, err := svc.FindAccountByID(1)
+	if err != nil {
+		t.Errorf("не удалось найти аккаунт, получили: %v", account)
+	}
+
+}
+
+func TestService_FindByAccountByID_notFound(t *testing.T) {
+	svc := Service{}
+	svc.RegisterAccount("+992938151007")
+	account, err := svc.FindAccountByID(2)
+	if err == nil {
+		t.Errorf("аккаунт найден, аккаунт: %v", account)
+	}
+
+}
+
+func TestService_Reject_success(t *testing.T) {
+	// создаем сервис
+	s := newTestService()
+
+	_, payments, err := s.addAcoount(defaultTestAccount)
+	if err != nil {
+		t.Error(err)
+		return
+	}
+
+	payment := payments[0]
+	err = s.Reject(payment.ID)
+
+	if err != nil {
+		t.Errorf("Reject(): error = %v", err)
+		return
+	}
+
+	savedPayment, err := s.FindPaymentByID(payment.ID)
+	if err != nil {
+		t.Errorf("Reject(): cant find payment by id, error = %v", err)
+		return
+	}
+	if savedPayment.Status != types.PaymentStatusFail {
+		t.Errorf("Reject(): status didnt changed, payment = %v", savedPayment)
+		return
+	}
+
+	savedAccount, err := s.FindAccountByID(payment.AccountID)
+	if err != nil {
+		t.Errorf("Reject(): cant find account by id, error = %v", err)
+		return
+	}
+	if savedAccount.Balance != defaultTestAccount.balance {
+		t.Errorf("Reject(): balance didnt changed, balance = %v", savedAccount)
+		return
+	}
+}
+
+func TestService_Repeat_success(t *testing.T) {
+	// создаем сервис
+	s := newTestService()
+
+	_, payments, err := s.addAcoount(defaultTestAccount)
+	if err != nil {
+		t.Error(err)
+		return
+	}
+
+	payment := payments[0]
+	newPayment, err := s.Repeat(payment.ID)
+
+	if err != nil {
+		t.Errorf("Repeat(): error = %v", err)
+		return
+	}
+
+	if newPayment.AccountID != payment.AccountID {
+		t.Errorf("Repeat(): error = %v", err)
+		return
+	}
+
+	if newPayment.Amount != payment.Amount {
+		t.Errorf("Repeat(): error = %v", err)
+		return
+	}
+
+	if newPayment.Category != payment.Category {
+		t.Errorf("Repeat(): error = %v", err)
+		return
+	}
+
+	if newPayment.Status != payment.Status {
+		t.Errorf("Repeat(): error = %v", err)
+		return
+	}
+
+}
+
+func TestService_FindFavoriteByID_succes(t *testing.T) {
+	s := newTestService()
+
+	_, payments, err := s.addAcoount(defaultTestAccount)
+	if err != nil {
+		t.Error(err)
+		return
+	}
+
+	payment := payments[0]
+	favorite, err := s.FavoritePayment(payment.ID, "ogastus")
+	if err != nil {
+		t.Errorf("favorite cant find, error = %v", err)
+	}
+
+	paymentFavorite, err := s.PayFromFavorite(favorite.ID)
+	if err != nil {
+		t.Errorf("PayFromFavorite() Error() can't for an favorite(%v), error = %v", paymentFavorite, err)
+	}
+}
